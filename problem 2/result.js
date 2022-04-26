5443684bd114486c0e11445e7c7436f09342cb4a
BovizLaci <92304557+BovizLaci@users.noreply.github.com>
2022-04-27 01:21:46 +0200

    finalize

--- problem 2/result.js
+++ problem 2/result.js
@@ -1,9 +1,9 @@
 function sumOfEvenFibonacciNumbersLessOrEqualThanMax(maxValue) {
+	let result = 2;
 	let fiboNumberPrev = 1; // 1st fibo number
 	let fiboNumber = 2; // 2nd fibo number
-	let result = 2;
-	let order = 0; // Every 3rd fibo numbers is even (2., 5., 8., etc)
 	let exceeded = false;
+	let order = 0; // Every 3rd fibo numbers is even (2., 5., 8., etc)
 	do {
 		const newFiboNumber = fiboNumber + fiboNumberPrev;
 		fiboNumberPrev = fiboNumber;
